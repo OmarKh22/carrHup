49c9fdb2781f660eb6b00fa586f078c339593243
Omarkh22 <ok597451@gmail.com>
2023-07-30 18:08:42 +0300

    the changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import CarCard from '@/components/CarCard'
 import Customfilter from '@/components/Customfilter'
 import Hero from '@/components/Hero'
 import SearchBar from '@/components/SearchBar'
+import { fuels, yearsOfProduction } from '@/constants'
+import { HomeProps } from '@/types'
+import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home({searchParams }: HomeProps) {
+
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year : searchParams.year || 2023,
+    fuel : searchParams.fuel || '',
+    limit : searchParams.limit || 10,
+    model : searchParams.model || '',
+  });
+  console.log(allCars)
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -18,10 +34,23 @@ export default function Home() {
           <SearchBar />
 
           <div className='home__filter-container'>
-            {/* <Customfilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} /> */}
+            <Customfilter title='fuel' options={fuels} />
+            <Customfilter title='year' options={yearsOfProduction} />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car)=> (<CarCard  car={car}/>))}
+            </div>
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oooops</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   )
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -36,7 +36,7 @@ const SearchBar = () => {
 
   const updateSearchParams = (model: string, manufacturer: string) => {
     // Create a new URLSearchParams object using the current URL search parameters
-    const searchParams = new URLSearchParams(window.location.search);
+    const searchParams = new URLSearchParams(window.location.search); 
 
     // Update or delete the 'model' search parameter based on the 'model' value
     if (model) {
