8e008917479dc531985d73930685b2844c828128
OmarKh22 <ok597451@gmail.com>
2023-08-03 22:48:22 +0300

    changge

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -7,7 +7,7 @@ import CustomButton from "./CustomButton";
 import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CarDetails from './CarDetails';
 
-//interface
+//interfacee
 interface CarCardProps {
     car: CarProps
 }
