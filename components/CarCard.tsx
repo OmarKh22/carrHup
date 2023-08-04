463e88d64df1b42e5e33d2a7d1f0dc253f09585e
OmarKh22 <ok597451@gmail.com>
2023-08-04 18:57:56 +0300

    Change

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -7,7 +7,7 @@ import CustomButton from "./CustomButton";
 import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CarDetails from './CarDetails';
 
-//interfaceeee
+//interface
 interface CarCardProps {
     car: CarProps
 }
